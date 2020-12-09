691cc1519a0f0dd8ef4f8b318ce40693d9b01ecf
Lev Michael <lev.choubine@gmail.com>
2020-12-09 10:26:40 -0500

    state

---
+++ state-and-class/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      count: 0
+    }
+    this.increment =this.increment.bind(this)
+  }
+
+increment () {
+    const currentCount = this.state.count
+    const newCount = currentCount + 1
+    this.setState({ count: newCount})
+  }
+    render(){
+      return (
+        <div>
+          窓の外雨は
+          <h1>{this.state.count}</h1>
+          <button onClick={this.increment}>+</button>
+        </div>
+      )
+    }
+ 
+  }
+
+
+
+export default App;
